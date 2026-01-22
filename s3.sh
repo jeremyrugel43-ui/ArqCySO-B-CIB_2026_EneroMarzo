e93b99684e7d043d0374167ef61a122b14065105
jeremyrugel43-ui <jeremyrugel43@gmail.com>
2026-01-22 01:17:15 +0000

    trabajo en proceso

--- s3.sh
+++ s3.sh
@@ -2,3 +2,14 @@
 echo -n "a" > letra.txt
 #   2. Ver su representación en bits (usando xxd para volcado binario)
 xxd -b letra.txt
+
+# Ver permisos actuales (mira la primera columna, ej: -rw-r--r--)
+ls -l letra.txt
+
+# Cambiar a permisos 755 (rwx para dueño, r-x para grupo, r-x para otros)
+chmod 755 letra.txt
+
+# Verificar cambiols
+ -l letra.txt
+
+ 
